4ab27ed5e8b75e854ebdb4b32a4ce04b8ae0d7eb
Neel Dalsania <neel.dalsania@astronomer.io>
2024-09-12 16:22:02 +0530

    fix mock file changes path

--- e2e-setup/mocks/git.sh
+++ e2e-setup/mocks/git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # hack to mock git commands as part of action.yaml so that we could simulate dags only deploy scenario without making any additional commits
-echo "e2e/astro-project/dags/exampledag.py"
+echo "e2e-setup/astro-project/dags/exampledag.py"
