b49f2b9fe4405394177b8209df4d0f89317f7ed0
neel-astro <neel.dalsania@astronomer.io>
2025-02-05 15:07:46 +0530

    fix git mocks

--- e2e-setup/mocks/image-deploy-git.sh
+++ e2e-setup/mocks/image-deploy-git.sh
@@ -7,6 +7,8 @@ if [[ "$1" == "diff" ]]; then
   echo "e2e-setup/astro-project/Dockerfile"
 elif [[ "$1" == "fetch" ]]; then
   echo "Handling git fetch, doing nothing"
+elif [[ "$1" == "cat-file" ]]; then
+  echo "Handling git cat-file, doing nothing"
 else
   echo "Error: git mock script isn't configured to handle $1" >&2
   exit 1
